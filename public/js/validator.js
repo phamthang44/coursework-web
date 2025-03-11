13923ae4167cc2cdd8e0f7e24d02da1fdf1aaff7
phamthang44 <Ducthang4342@gmail.com>
2025-03-11 08:51:52 +0700

    add validator by js

---
+++ public/js/validator.js
@@ -0,0 +1,208 @@
+function Validator(options) {
+  function getParent(element, selector) {
+    while (element.parentElement) {
+      if (element.parentElement.matches(selector)) {
+        return element.parentElement;
+      }
+      element = element.parentElement; //VIP
+    }
+  }
+
+  let selectorRules = {};
+
+  function validate(inputElement, rule) {
+    let errorMessage;
+
+    let errorElement = getParent(
+      inputElement,
+      options.formGroupSelector
+    ).querySelector(options.formMessage);
+
+    let rules = selectorRules[rule.selector];
+
+    for (let i = 0; i < rules.length; i++) {
+      switch (inputElement.type) {
+        case "checkbox":
+          errorMessage = rules[i](
+            formElement.querySelector(rule.selector + ":checked")
+          );
+          break;
+        case "radio":
+          errorMessage = rules[i](
+            formElement.querySelector(rule.selector + ":checked")
+          );
+          break;
+        default:
+          errorMessage = rules[i](inputElement.value);
+      }
+
+      if (errorMessage) break;
+    }
+
+    if (errorMessage) {
+      errorElement.innerText = errorMessage;
+      getParent(inputElement, options.formGroupSelector).classList.add(
+        "invalid"
+      );
+    } else {
+      errorElement.innerText = "";
+      getParent(inputElement, options.formGroupSelector).classList.remove(
+        "invalid"
+      );
+    }
+
+    return !errorMessage;
+  }
+
+  let formElement = document.querySelector(options.form);
+
+  if (formElement) {
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
+
+      let isFormValid = true;
+
+      options.rules.forEach((rule) => {
+        let inputElement = formElement.querySelector(rule.selector);
+        validate(inputElement, rule);
+        let isValid = validate(inputElement, rule);
+
+        if (!isValid) {
+          isFormValid = false;
+        }
+      });
+
+      if (isFormValid) {
+        if (typeof options.onSubmit === "function") {
+          let enableInputs = formElement.querySelectorAll(
+            "[name]:not([disabled])"
+          );
+          //console.log(enableInputs); //NodeList
+
+          let formValues = Array.from(enableInputs).reduce((values, input) => {
+            switch (input.type) {
+              case "radio":
+                values[input.name] = formElement.querySelector(
+                  'input[name="' + input.name + '"]:checked'
+                ).value;
+                break;
+              case "checkbox":
+                if (!input.matches(":checked")) return (values = "");
+                if (!Array.isArray(values[input.name])) {
+                  values[input.name] = [];
+                }
+                values[input.name].push(input.value);
+                break;
+              case "file":
+                values[input.name] = input.files;
+                break;
+              default:
+                values[input.name] = input.value;
+            }
+
+            return values;
+          }, {});
+
+          options.onSubmit(formValues);
+        } else {
+          formElement.submit();
+        }
+      }
+    };
+
+    options.rules.forEach((rule) => {
+      let inputElements = formElement.querySelectorAll(rule.selector);
+
+      Array.from(inputElements).forEach(function (inputElement) {
+        inputElement.onblur = function () {
+          validate(inputElement, rule);
+        };
+
+        inputElement.oninput = function () {
+          let errorElement = getParent(
+            inputElement,
+            options.formGroupSelector
+          ).querySelector(options.formMessage);
+          errorElement.innerText = "";
+          getParent(inputElement, options.formGroupSelector).classList.remove(
+            "invalid"
+          );
+        };
+      });
+
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+    });
+  }
+}
+
+Validator.isRequired = function (selector) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value ? undefined : "Please input this field";
+    },
+  };
+};
+
+Validator.isRequiredSelection = function (selector) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value ? undefined : "Please select one of these options";
+    },
+  };
+};
+
+Validator.isEmail = function (selector, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return regex.test(value)
+        ? undefined
+        : message || "This field must be an email";
+    },
+  };
+};
+
+Validator.minLength = function (selector, min, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.length >= min
+        ? undefined
+        : message || `Please input at least ${min} characters`;
+    },
+  };
+};
+
+Validator.isConfirmed = function (selector, getConfirmedValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value === getConfirmedValue()
+        ? undefined
+        : message || "The confirmation does not match with the password!";
+    },
+  };
+};
+
+Validator.isRequiredDate = function (selector) {
+  return {
+    selector: selector,
+    test: function () {},
+  };
+};
+
+Validator.hasNoWhiteSpace = function (selector) {
+  return {
+    selector: selector,
+    test: function (s) {
+      return !/\s/g.test(s) ? undefined : "Please do not use white space";
+    },
+  };
+};
--- src/views/posts/createpost.php
+++ src/views/posts/createpost.php
@@ -36,17 +36,19 @@
 <body>
     <div class="form-container">
         <h2>Upload Form</h2>
-        <form action="index.php?action=store" method="POST" enctype="multipart/form-data">
+        <form action="index.php?action=store" method="POST" enctype="multipart/form-data" id="form-upload-post">
             <!-- Title Field (Optional) -->
             <div class="form-group">
                 <label for="title">Title (Optional):</label>
                 <input type="text" id="title" name="title" placeholder="Enter title (optional)">
+                <span class="form-message"></span>
             </div>
 
             <!-- Content Field (Required) -->
             <div class="form-group">
                 <label for="content">Content (Required):</label>
                 <textarea id="content" name="content" rows="5" required placeholder="Enter content"></textarea>
+                <span class="form-message"></span>
             </div>
 
             <!-- Module Select -->
@@ -58,12 +60,14 @@
                         <option value="<?php echo $module->getModuleId(); ?>"><?php echo $module->getModuleName(); ?></option>
                     <?php endforeach; ?>
                 </select>
+                <span class="form-message"></span>
             </div>
 
             <!-- Image Upload -->
             <div class="form-group">
                 <label for="image">Upload Image:</label>
                 <input type="file" id="image" name="image" accept="image/*">
+                <span class="form-message"></span>
             </div>
 
             <!-- Submit Button -->
@@ -71,7 +75,21 @@
                 <input type="submit" value="Submit">
             </div>
         </form>
+
     </div>
+    <script src="/js/validator.js"></script>
+    <script>
+        Validator({
+            form: "#form-upload-post",
+            formGroupSelector: ".form-group",
+            formMessage: ".form-message",
+            rules: [
+                Validator.isRequired("#username"),
+                Validator.isRequired("#content"),
+                Validator.isRequiredSelection("#module"),
+            ],
+        });
+    </script>
 </body>
 
 </html>
