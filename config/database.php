8b0e8ddf7b828d4553961e8ca0a154c6e16370fc
phamthang44 <Ducthang4342@gmail.com>
2025-04-18 10:41:28 +0700

    update: change name file

--- config/database.php
+++ config/database.php
@@ -1,23 +1,27 @@
 <?php
-    namespace database;
-    use \PDO;
-    use \PDOException;
-    class Database {
-        private $host = "localhost";
-        private $db_name = "student_stack_overflow";
-        private $username = "root";
-        private $password = "phamthang20042005";
-        private $conn;
-        
-        public function getConnection() {
-            try {
-                $this->conn = new PDO("mysql:host=".$this->host.";port=3307;dbname=".$this->db_name, $this->username, $this->password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $this->conn;
-            } catch (PDOException $e) {
-                echo "Connection failed: " . $e->getMessage();
-                return null;
-            }
+
+namespace database;
+
+use \PDO;
+use \PDOException;
+
+class Database
+{
+    private $host = "localhost";
+    private $dbName = "student_stack_overflow";
+    private $username = "root";
+    private $password = "phamthang20042005";
+    private $conn;
+
+    public function getConnection()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";port=3307;dbname=" . $this->dbName, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->conn;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            return null;
         }
     }
-?>
+}
